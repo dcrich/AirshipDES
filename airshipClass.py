7de8cac54888a847c4c3c37f09a9c470416a2aef
dcrich <43016577+dcrich@users.noreply.github.com>
2022-01-24 21:15:17 -0700

    airship class basics

--- airshipClass.py
+++ airshipClass.py
@@ -11,41 +11,59 @@ class Airship:
         self.CurrentDay = 0
         self.StartHourWorkday = StartHourWorkday
         self.EndHourWorkday = EndHourWorkday
-        self.hub = hub
-        self.cities = cities
-        self.StillWorkday = True
-        # Airship Design Parameters
-        
-        # airship capabilities/capacities
+        self.Hub = hub
+        self.Cities = cities
+
+        # airship design parameters
         self.UsefulPayload = airshipAttributes[0]
         self.FuelCapacity = airshipAttributes[1]
-        self.FuelLevel = airshipAttributes[1]
         self.Footprint = airshipAttributes[2]
+        self.CruiseSpeed = airshipAttributes[3]
+        self.AirshipVolume_ft = airshipAttributes[4]
 
         # tracking variables
-        self.PayloadLevel = 0.0
+        self.PayloadRemaining = airshipAttributes[0]
         self.GoodsTransported = np.zeros(360)
-        self.FuelLevel = airshipAttributes[1]
+        self.FuelRemaining = airshipAttributes[1]
         self.FuelUsed = 0.0
+        
+        # status variables
+        self.StillWorkday = True
+        self.AtHub = True
+        self.CitySelected = False
+        self.CurrentLocation = [0,0]
+        self.NextCity = 0
 
 
+        # queue simulation for airship instance
+        self.env.process(self.start_working)
     # Action Functions
-    def working(self): # main operation loop
+    def start_working(self): # main operation loop
         while True: # run until sim is over
-            self.start_working()
-            self.stop_working()
+            yield self.env.process(self.working())
+            yield self.env.process(self.stop_working())
 
-    def start_working(self):
-        while self.env.now < self.EndHourWorkday:
-            self.hub_to_city()
-            self.city_to_hub()
+    def working(self):
+        while self.env.now < self.EndHourWorkday: # work until end of the day
+            self.choose_city()
+            
+            if self.AtHub and self.CitySelected: # at hub, city choice
+                yield self.env.process(self.hub_to_city())
+            
+            elif self.AtHub and not self.CitySelected: # at hub, no city chosen
+                break # leave loop and to go to stop working
+            
+            elif not self.AtHub and self.CitySelected: # not at hub, city chosen
+                yield self.env.process(self.city_to_city())
+           
+            elif not self.AtHub and not self.CitySelected: # not at hub and no city chosen
+                yield self.env.process(self.city_to_hub())
 
     def stop_working(self):
-        gotosleep = 1
-
+        self.work_clock()
+        yield self.env.timeout(self.StartHourWorkday) # do nothing working until next day
+        
     def hub_to_city(self):
-        # choose city
-        self.choose_city()
         # fly
         # land, load cargo, etc.
         self.choose_city_activity()
@@ -73,12 +91,41 @@ class Airship:
     
     
     # Behavioral/Decision Functions
-    def in_range_time(self):
-        check = 1
-    def in_range_fuel(self):
-        check = 1
     def choose_city(self):
-        choose = 1
+        if not self.StillWorkday:
+            self.CitySelected = False
+        else:
+            for i in range(len(self.Cities)):
+                self.next_city()
+                self.in_range()
+                if self.InRange:
+                    self.CitySelected = True
+                    break
+                else:
+                    self.CitySelected = False
+
+    def next_city(self):
+        self.NextCity +=1
+        if self.NextCity > len(self.Cities):
+            self.NextCity = 0
+    
+    def in_range(self):
+        fuelSafetyFactor = 1.1 # 10% safety factor
+        distanceToDestination = self.distance_between_coordinates(self.CurrentLocation, self.Cities[self.NextCity].LatLon)
+        distanceToHubFromNextCity = self.distance_between_coordinates(self.Cities[self.NextCity].LatLon, self.Hub.LatLon)
+        timeToDestination = distanceToDestination / self.CruiseSpeed
+        timeToHubFromNextCity = distanceToHubFromNextCity / self.CruiseSpeed
+        fuelToDestination = self.calculate_fuel_used(timeToDestination)
+        fuelToHubFromNextCity = self.calculate_fuel_used(timeToHubFromNextCity)
+        InRangeFuel = (fuelToDestination + fuelToHubFromNextCity) * fuelSafetyFactor < self.FuelRemaining
+        timeToLoad = self.PayloadRemaining / self.Cities[self.NextCity].LoadingRate
+        InRangeTime = (timeToDestination + timeToHubFromNextCity + timeToLoad) < (self.EndHourWorkday - self.env.now)
+        
+        if InRangeFuel and InRangeTime:
+            self.InRange = True
+        else:    
+            self.InRange = False
+   
     def choose_city_activity(self):
         self.load_cargo()
 
@@ -88,8 +135,44 @@ class Airship:
 
     # General Calculations
     def work_clock(self):
-        time = 1
-        self.CurrentDay = 1
+        self.CurrentDay += 1
+        self.StartHourWorkday += 24
+        self.EndHourWorkday += 24 
 
-    def calculate_range(self):
-        numberofkm = 1
+    def calculate_fuel_used(self,cruiseTime):
+        # in imperial units
+        # fuelTankSize_imperial = 1.1 * self.FuelTankSize #convert to imperial
+        propEfficiency = 0.7  #prop efficiency
+        dragCoeff = 0.025   # Drag Coefficient
+        SFC = 0.41    #lb/hp/hr
+        headwindSpeed_knots = 10    # knots headwind
+        speedKnots = self.CruiseSpeed * 0.539957    # knots cruise speed
+        totalAirshipVolume = self.AirshipVolume_ft * 10 ** -6    #M ft**3
+
+         #Constants and Conversion Factors
+        knots2fps = 1.68780986 
+        lbsPerSlug = 32.1740486 
+        hpPerFtLbPerSec = 550 
+        rhoAir = np.NaN #densityAltitude at 2000 ft
+
+        # Calculate fuel per mile based on cruise speed, altitude, and wind
+        v = speedKnots * knots2fps    # ft/s cruise speed
+        HpFuelCalc = dragCoeff * (totalAirshipVolume * 10 ** 6) ** (2 / 3) * rhoAir *        \
+                        v ** 3 / (2 * propEfficiency * hpPerFtLbPerSec * lbsPerSlug)    #propulsion horsepower
+        tonsPerHour = HpFuelCalc * SFC / 2000    # tons of fuel per hour
+        fuelUsed = tonsPerHour * cruiseTime
+        
+        # tonsPerMile = tonsPerHour / (speedKnots - headwindSpeed_knots)    #tons of fuel per mile        
+        # safetyFactor = 1.1
+        # self.Range_km = fuelTankSize_imperial / (safetyFactor * tonsPerMile)
+        return fuelUsed
+    
+    def distance_between_coordinates(latlon1, latlon2):
+        lat1 = latlon1[0] * np.pi / 180
+        lon1 = latlon1[1] * np.pi / 180
+        lat2 = latlon2[0] * np.pi / 180
+        lon2 = latlon2[1] * np.pi / 180
+        # distance in nm
+        distanceBetweenCoord = 3440 * 2 * np.arcsin(np.sqrt((np.sin((lat1-lat2)/2))**2 + 
+                                np.cos(lat1)*np.cos(lat2)*(np.sin((lon1-lon2)/2))**2))
+        return distanceBetweenCoord
